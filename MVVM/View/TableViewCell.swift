eb009f9a659771a49e6bf6c8f1cddb9b0379c631
Valery <ihValery@gmail.com>
2021-04-20 18:57:33 +0300

    Таблица с архитектурой MVC работает (переделываем под MVVM)

---
+++ MVVM/View/TableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewCell.swift
+//  MVVM
+//
+//  Created by Валерий Игнатьев on 20.04.21.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+}
