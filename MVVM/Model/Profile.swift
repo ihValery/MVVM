a5e5a65c41f01372b0909c9684dfa24cca4fc592
Valery <ihValery@gmail.com>
2021-04-20 04:59:35 +0300

    Initial Commit

---
+++ MVVM/Model/Profile.swift
@@ -0,0 +1,15 @@
+//
+//  Profile.swift
+//  MVVM
+//
+//  Created by Валерий Игнатьев on 20.04.21.
+//
+
+import Foundation
+
+struct Profile {
+    
+    var name: String
+    var surName: String
+    var age: Int
+}
---
+++ MVVM/View/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by Валерий Игнатьев on 20.04.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var suRameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    
+    var viewModel: ViewModel! {
+        didSet {
+            self.nameLabel.text = viewModel.name
+            self.suRameLabel.text = viewModel.surName
+            self.ageLabel.text = viewModel.age
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel = ViewModel()
+    }
+}
